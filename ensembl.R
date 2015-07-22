d7d3dcac20d524d75b78d1e48b389814c0f42762
JakeConway <jakeconway45@gmail.com>
2015-07-21 23:28:40 -0400

    Added progress bars

--- ensembl.R
+++ ensembl.R
@@ -1,75 +1,91 @@
 library(UpSetR)
 library(biomaRt)
 library(data.table)
-UpSetRensembl <- function(organisms){
-  species <- read.csv("genes.csv")
-  species <- species[which(species$description %in% organisms),]
-  species <- head(as.character(species$dataset), 5)
+# UpSetRensembl <- function(organisms){
+#   species <- read.csv("genes.csv")
+#   species <- species[which(species$description %in% organisms),]
+#   species <- head(as.character(species$dataset), 5)
+
   
-  getEnsemblMarts <- function(dSet){
-    ensembl <- useMart("ENSEMBL_MART_ENSEMBL",host = "useast.ensembl.org", dataset=dSet)
-  }
+#   sixSpecies <- function(species){
+#     if(length(species) > 6){warning("6 species maximum")}
+#     else{
+#       datasets <- list()
+#       for(i in seq_along(species)){
+#         datasets[[i]] <- getEnsemblMarts(species[i])
+#       }
+#     }
+#     return(datasets)
+#   }
   
-  sixSpecies <- function(species){
-    if(length(species) > 6){warning("6 species maximum")}
-    else{
-      datasets <- list()
-      for(i in seq_along(species)){
-        datasets[[i]] <- getEnsemblMarts(species[i])
-      }
+  # data <- sixSpecies(species)
+  
+  
+  # namesData <- getNames(species)
+  
+#   grabData <- function(data, namesData, species){
+#     for(i in seq_along(data)){
+#       data[[i]] <- getBM(attributes = c("ensembl_gene_id",namesData[[i]]), mart = data[[i]])
+#       new_colname <- paste(unlist(strsplit(species[i], split = "_"))[1], "_homolog_ensembl_gene", sep = "", collapse="")
+#       colnames(data[[i]])[1] <- new_colname
+#       data[[i]] <- data[[i]][ ,order(colnames(data[[i]]))]
+#     }
+#     alldata <- rbindlist(data)
+#     alldata <- data.frame(alldata[!duplicated(alldata),])
+#     return(alldata)
+#   }
+#   data <- grabData(data, namesData, species)
+  
+  
+#   data <- BioMartConverter(data)
+#   
+#   upset(data, order.matrix="freq", empty.intersections = "on")
+# }
+
+getEnsemblMarts <- function(dSet){
+  ensembl <- useMart("ENSEMBL_MART_ENSEMBL",host = "useast.ensembl.org", dataset=dSet)
+}
+
+getNames <- function(species){
+  allnames <- list()
+  cnames <- c()
+  for(i in seq_along(species)){
+    current <- species[i]
+    names <- species[which(!species %in% current)]
+    for(j in seq_along(names)){
+      name <- strsplit(names[j], split = "_")
+      cnames[j] <- unlist(name)[1]
+      cnames[j] <- paste(cnames[j], "_homolog_ensembl_gene", sep = "", collapse = "")
     }
-    return(datasets)
+    allnames[[i]] <- cnames
   }
-  
-  data <- sixSpecies(species)
-  
-  getNames <- function(species){
-    allnames <- list()
-    cnames <- c()
-    for(i in seq_along(species)){
-      current <- species[i]
-      names <- species[which(!species %in% current)]
-      for(j in seq_along(names)){
-        name <- strsplit(names[j], split = "_")
-        cnames[j] <- unlist(name)[1]
-        cnames[j] <- paste(cnames[j], "_homolog_ensembl_gene", sep = "", collapse = "")
-      }
-      allnames[[i]] <- cnames
-    }
-    return(allnames)
+  return(allnames)
+}
+
+# grabData <- function(data, namesData, species){
+#   for(i in seq_along(data)){
+#     data[[i]] <- getBM(attributes = c("ensembl_gene_id",namesData[[i]]), mart = data[[i]])
+#     new_colname <- paste(unlist(strsplit(species[i], split = "_"))[1], "_homolog_ensembl_gene", sep = "", collapse="")
+#     colnames(data[[i]])[1] <- new_colname
+#     data[[i]] <- data[[i]][ ,order(colnames(data[[i]]))]
+#   }
+#   alldata <- rbindlist(data)
+#   alldata <- data.frame(alldata[!duplicated(alldata),])
+#   return(alldata)
+# }
+
+
+BioMartConverter <- function(data){
+  data <- data[which(!duplicated(data)), ]
+  sets <- grep(tolower("homolog"), colnames(data))
+  setnames <- colnames(data[c(sets)]) 
+  attnames <- NULL
+  if(length(setnames) != length(data)){
+    attnames <-colnames(data[-c(sets)])
   }
-  
-  namesData <- getNames(species)
-  grabData <- function(data, namesData, species){
-    for(i in seq_along(data)){
-      data[[i]] <- getBM(attributes = c("ensembl_gene_id",namesData[[i]]), mart = data[[i]])
-      new_colname <- paste(unlist(strsplit(species[i], split = "_"))[1], "_homolog_ensembl_gene", sep = "", collapse="")
-      colnames(data[[i]])[1] <- new_colname
-      data[[i]] <- data[[i]][ ,order(colnames(data[[i]]))]
-    }
-    alldata <- rbindlist(data)
-    alldata <- data.frame(alldata[!duplicated(alldata),])
-    return(alldata)
-  }
-  data <- grabData(data, namesData, species)
-  
-  
-  BioMartConverter <- function(data){
-    data <- data[which(!duplicated(data)), ]
-    sets <- grep(tolower("homolog"), colnames(data))
-    setnames <- colnames(data[c(sets)]) 
-    attnames <- NULL
-    if(length(setnames) != length(data)){
-      attnames <-colnames(data[-c(sets)])
-    }
-    names <- c(attnames, setnames)
-    data <- data.frame(apply(data[sets], 1:2, function(x) if(isTRUE(x == "")){x <- 0} else{x=1}))
-    #cbind(data[ ,!(colnames(data) %in% colnames(data[,sets]))],
-    colnames(data) <- names
-    return(data)
-  }
-  
-  data <- BioMartConverter(data)
-  
-  upset(data, order.matrix="freq", empty.intersections = "on")
+  names <- c(attnames, setnames)
+  data <- data.frame(apply(data[sets], 1:2, function(x) if(isTRUE(x == "")){x <- 0} else{x=1}))
+  #cbind(data[ ,!(colnames(data) %in% colnames(data[,sets]))],
+  colnames(data) <- names
+  return(data)
 }
--- server.R
+++ server.R
@@ -10,6 +10,7 @@ shinyServer(function(input, output, session){
     return(names)
   })
   
+  
   output$genes <- renderUI({
     genes <- selectInput('Select', h6("Select organisms:"),
                          choices = as.character(datasets()),
@@ -26,6 +27,61 @@ shinyServer(function(input, output, session){
   as.character(input$Select)
 })
 
+  species <- reactive({
+    species <- read.csv("genes.csv")
+    species <- species[which(species$description %in% organisms()),]
+    species <- head(as.character(species$dataset), 5)
+  })
+  
+  data <- reactive({
+   m <- (length(species())/10)
+      if(length(species()) > 6){warning("6 species maximum")}
+      else{
+        datasets <- list()
+        withProgress(message = 'Selecting data sets from Biomart', value = 0, max = m,
+                     detail = "Data set 0", {
+          Sys.sleep(0.1)
+        for(i in seq_along(species())){
+          incProgress(0.1, detail = paste("Data set", i))
+          datasets[[i]] <- getEnsemblMarts(species()[i])
+        }
+       })
+      }
+      return(datasets)
+  })
+  
+  namesData <- reactive({
+   namesData <- getNames(species())
+   return(namesData)
+  })
+  
+  homologs <- reactive({
+    homologs <- list()
+    m <- (length(species())/10)
+    withProgress(message = 'Querying data sets from BioMart Website', value = 0, max =m,
+                 detail= 'Querying on data set 0', {
+                   Sys.sleep(0.1)
+      for(i in seq_along(data())){
+        incProgress(0.1, detail = paste("Querying on data set", i))
+        homologs[[i]] <- getBM(attributes = c("ensembl_gene_id",namesData()[[i]]), mart = data()[[i]])
+        new_colname <- paste(unlist(strsplit(species()[i], split = "_"))[1], "_homolog_ensembl_gene", sep = "", collapse="")
+        colnames(homologs[[i]])[1] <- new_colname
+        homologs[[i]] <- homologs[[i]][ ,order(colnames(homologs[[i]]))]
+      }
+    })
+      alldata <- rbindlist(homologs)
+      alldata <- data.frame(alldata[!duplicated(alldata),])
+      return(alldata)
+  })
+  
+  upsetdata <- reactive({
+    withProgress(message = 'Converting to upset data', value = 0, {
+      upsetdata <- BioMartConverter(homologs())
+      setProgress(1)
+      return(upsetdata)
+    })
+  })
+  
 output$plot_text <- renderUI({
   if(length(input$Select) == 0){
   text1 <- "This is where your plot will be displayed."
@@ -51,7 +107,7 @@ output$plot <- renderImage({
   png(outfile, width=width*pixelratio, height=height*pixelratio,
       res=72*pixelratio)
   if(!is.null(organisms()))
-    UpSetRensembl(organisms())
+    upset(upsetdata(), order.matrix="freq", empty.intersections = "on")
   
   dev.off()
   # Return a list
